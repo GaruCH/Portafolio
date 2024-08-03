c256ea773941088649f327919bf6b8af4e362f40
GaruCH <146020588+GaruCH@users.noreply.github.com>
2024-08-03 01:26:27 -0600

    v3

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    loadComponent('header', '../components/header.html');
-    loadComponent('navbar', '../components/navbar.html');
-    loadComponent('footer', '../components/footer.html');
-});
-
-function loadComponent(elementId, url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(elementId).innerHTML = data;
-        })
-        .catch(error => console.error('Error loading component:', error));
-}
-
 
 window.addEventListener('DOMContentLoaded', event => {
 
