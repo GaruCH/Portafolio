508bc81f58d1484c1f620caa31b6102e19df3382
GaruCH <146020588+GaruCH@users.noreply.github.com>
2025-01-30 01:00:57 -0600

    Nueva version del portafolio

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
-const carousel = document.getElementById('carousel');
+// Menú responsive
+const menuBtn = document.getElementById('menu-btn');
+const sidebar = document.getElementById('sidebar');
+
+
+// Al hacer clic en el botón del menú, alternamos la clase 'show'
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('hidden');
+  menuBtn.classList.toggle('border-4'); // Mostramos el menú dentro de la pantalla
+  sidebar.classList.toggle('-translate-x-full');  // Movemos el menú dentro de la pantalla
+});
+
+// Cerrar el menú si se hace clic en el fondo oscuro
+sidebar.addEventListener('click', (e) => {
+  if (e.target === sidebar) {
+    sidebar.classList.add('hidden');
+    sidebar.classList.add('-translate-x-full');  // Volver a esconder el menú
+  }
+});
+
+feather.replace();
+
+/*const carousel = document.getElementById('carousel');
 const slides = carousel.children;
 const totalSlides = slides.length;
 
@@ -20,3 +42,4 @@ document.getElementById('next').addEventListener('click', () => {
   currentIndex = (currentIndex + 1) % totalSlides;
   updateCarousel();
 });
+*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        primary: '#000000',
-        secondary: '#92DCE5',
-        tertiary: '#D64933',
+        primary: "#027373",   // Verde oscuro
+        secondary: "#038C7F", // Verde medio
+        accent: "#A9D9D0",    // Verde claro
+        background: "#F2E7DC", // Crema (fondo en modo claro)
+        darkbg: "#0D0D0D",    // Negro (fondo en modo oscuro)
+        textlight: "#0D0D0D", // Texto oscuro para modo claro
+        textdark: "#F2E7DC",  // Texto claro para modo oscuro
+      },
+      fontFamily: {
+        josefin: [ "Josefin Sans", "serif"],
       },
     },
   },
